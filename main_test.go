a7c0e77edc56e1db0d6fd7db3ba22b7a849c66ba
insanitywholesale <popomastor@gmail.com>
2022-03-08 22:21:57 +0200

    go fmt

--- main_test.go
+++ main_test.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"testing"
 	"ctrlshiftv/paste"
+	"testing"
 )
 
 // Test is still incomplete
@@ -16,20 +16,20 @@ func TestGet(t *testing.T) {
 	// make redirect service
 	service := paste.NewPasteService(repo)
 	t.Log("service", service)
-/*
-	// create router based on the above service
-	r := makeRouter(service)
-	// create and start a test server
-	testServer := httptest.NewServer(r)
-	// do a simple Get request on preexisting redirect
-	// said redirect can be found in the mock repo source
-	res, err := http.Get(testServer.URL + "/1234")
-	// be responsible and close the response body
-	res.Body.Close()
-	if err != nil {
-		t.Error("tfw GET error:", err)
-	}
-	// close the test server
-	testServer.Close()
-*/
+	/*
+		// create router based on the above service
+		r := makeRouter(service)
+		// create and start a test server
+		testServer := httptest.NewServer(r)
+		// do a simple Get request on preexisting redirect
+		// said redirect can be found in the mock repo source
+		res, err := http.Get(testServer.URL + "/1234")
+		// be responsible and close the response body
+		res.Body.Close()
+		if err != nil {
+			t.Error("tfw GET error:", err)
+		}
+		// close the test server
+		testServer.Close()
+	*/
 }
